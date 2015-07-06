87892af2be31b6e6449715755e7c133b0b14e413
redo2011dht <redo2011dht@gmail.com>
2015-07-06 13:42:13 +0700

    created scaffold Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706063729) do
+ActiveRecord::Schema.define(version: 20150706064127) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "likenumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
